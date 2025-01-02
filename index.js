628ac195704ab46f3cb10ca24ffa95fc7c1529d1
UltimateQuack <patoflamejantetv@gmail.com>
2025-01-02 23:59:14 +0000

    v1 :0 :3333333

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+
+app.get('/', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Please go to /random-image.');
+});
+
+app.get('/random-image', (req, res) => {
+  const images = fs.readdirSync(path.join(__dirname, 'assets'));
+  const randomIndex = Math.floor(Math.random() * images.length);
+  const randomImage = images[randomIndex];
+  res.sendFile(path.join(__dirname, 'assets', randomImage));
+});
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
