89b23000733bfbf432b66694b2472593a2d19a0a
UltimateQuack <patoflamejantetv@gmail.com>
2025-01-04 16:30:21 +0000

    IMAGE_ONLY update

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 
-const images = fs.readdirSync(path.join(__dirname, 'assets'));
+const IMAGE_ONLY = true; // If true, videos will NOT show, useful in case you are using this on your GitHub profile.
+
+const images = fs.readdirSync(path.join(__dirname, 'assets')).filter(file => {
+  if (IMAGE_ONLY) {
+    return path.extname(file).toLowerCase() === '.jpg' || path.extname(file).toLowerCase() === '.png' || path.extname(file).toLowerCase() === '.jpeg' || path.extname(file).toLowerCase() === '.gif' || path.extname(file).toLowerCase() === '.webp';
+  } else {
+    return true;
+  }
+});
 
 app.get('/', (req, res) => {
   const randomIndex = Math.floor(Math.random() * images.length);
